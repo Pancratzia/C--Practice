cd9914c56b05dcaab764d43bfff6f6673d8ee5ef
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-29 10:35:20 -0400

    Aprendineo que las funciones se pueden pasar como parametros

--- ConsoleFirstProject/Program.cs
+++ ConsoleFirstProject/Program.cs
@@ -2,7 +2,17 @@
 var show = Show;
 
 show("Hello World!");
+
+Some(Show, "Some message");
 void Show(string message)
 {
     Console.WriteLine(message);
+}
+
+void Some(Action<string> fn, string message)
+{
+    Console.WriteLine("Hola!");
+    fn(message);
+
+    Console.WriteLine("Adios!");
 }
