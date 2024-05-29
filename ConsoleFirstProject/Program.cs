911eb4ff0ede53a55ddaa31448e0ee27c68e9526
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-29 10:33:39 -0400

    Aprendiendo que pueden guardarse funciones dentro de una variable como en JS

--- ConsoleFirstProject/Program.cs
+++ ConsoleFirstProject/Program.cs
@@ -1,25 +1,8 @@
-﻿var beer = new Beer()
+﻿
+var show = Show;
+
+show("Hello World!");
+void Show(string message)
 {
-    Name = "Corona"
-};
-
-
-Console.WriteLine(ToUpper(beer).Name);
-Console.WriteLine(beer.Name);
-
-
-
-Beer ToUpper (Beer beer) //Es pura, ya que crea una nueva instancia y no modifica el valor original
-{
-    var newBeer = new Beer()
-    {
-        Name = beer.Name.ToUpper()
-    };
-
-    return newBeer;
-}
-
-class Beer
-{
-    public string Name { get; set; }
+    Console.WriteLine(message);
 }
