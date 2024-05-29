d7dc2d2a523b56b78e711de940132421a7ec4c6c
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-29 10:09:36 -0400

    Aprendiendo sobre funciones puras

--- ConsoleFirstProject/Program.cs
+++ ConsoleFirstProject/Program.cs
@@ -1,33 +1,17 @@
-﻿using System.Text.Json;
+﻿Console.WriteLine("La resta de 10 menos 5 da como resultado " + Sub(10, 5));
 
-var pancratzia = new People()
+
+int Sub(int a, int b) //Función Pura. Siempre devuelve un el mismo valor bajo las mismas condiciones en su llamada
 {
-    Name = "Pancratzia",
-    Age = 23
-};
+    return a - b;
+}
 
-string json = JsonSerializer.Serialize(pancratzia);
-Console.WriteLine(json);
-
-
-string myJSON = @"{
-""Name"":""Pancratzia"",
-""Age"":23
-}";
-
-var pancratzia2 = JsonSerializer.Deserialize<People>(myJSON);
-Console.WriteLine(pancratzia2?.Name);
-Console.WriteLine(pancratzia2?.Age);
-
-People? pancratzia3 = null;
-
-Console.WriteLine(pancratzia3?.Name);
-Console.WriteLine(pancratzia3?.Age);
-
-
-public class People
+DateTime GetTommorrow() //No es una función pura porque modifica el tiempo
 {
-    public string Name { get; set; }
-    public int Age { get; set; }
+    return DateTime.Now.AddDays(1);
+}
 
+DateTime GetTommorrowPure(DateTime date) //Función pura debido a que si se le pasa el mismo parametro todos los días, siempre dará el mismo valor
+{
+    return date.AddDays(1);
 }
