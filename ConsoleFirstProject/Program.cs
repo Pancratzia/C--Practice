2c5b36c88e0d8cbac188fb9b725dccac8c5ee9f2
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-29 10:57:01 -0400

    Aprendiendo equivalente de LINQ

--- ConsoleFirstProject/Program.cs
+++ ConsoleFirstProject/Program.cs
@@ -4,10 +4,13 @@
 };
 
 var namesResult = from n in names
+                  where n.Length > 3 && n.Length<=6
                   orderby n descending
                   select n;
 
-foreach (var name in namesResult)
+var namesResult2 = names.Where(n=>n.Length > 3 && n.Length<=6).OrderByDescending(n=>n).Select(d=>d);
+
+foreach (var name in namesResult2)
 {
     Console.WriteLine(name);
 }
