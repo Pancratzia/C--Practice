746e464fd18c6b9029ddcf21dc5f86cc8608f01b
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-29 10:50:25 -0400

    Aprendiendo la utilizad de las funciones lambda

--- ConsoleFirstProject/Program.cs
+++ ConsoleFirstProject/Program.cs
@@ -1,16 +1,7 @@
-﻿Func<int, int, int> sub = (a,b) => a - b;
-Func<int, int> doubleNum = x => x * 2;
-Func<int, int> tripleNum = x =>
+﻿Console.WriteLine(Some((a, b) => a + b, 1));
+
+int Some (Func <int, int, int> fn, int number)
 {
-    int aux = 3;
-    return x * aux;
-};
-
-int result = sub(10, 5);
-Console.WriteLine(result);
-
-result = doubleNum(result);
-Console.WriteLine(result);
-
-result = tripleNum(result);
-Console.WriteLine(result);
+    var result = fn(number, number);
+    return result;
+}
